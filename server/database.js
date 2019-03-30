013742261625905f2a389d84c805fd88f3b61acd
Andrei Sukharev <mr.andrey.sd@gmail.com>
2019-03-30 13:02:00 +0300

    4

--- server/database.js
+++ server/database.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/api', {useNewUrlParser: true, useFindAndModify: false });
+mongoose.connect('mongodb://localhost/api_sukharev', {useNewUrlParser: true, useFindAndModify: false });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('we are connected!');
+  console.log('we are connected to mongoDB!');
 });
 
 // To load and start the MongoDB background service, open Terminal and execute the following commands :
